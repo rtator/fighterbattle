ce300ac14dbf6735b430423fc908f19e6841c1a3
rtator <66912559+rtator@users.noreply.github.com>
2021-02-20 21:11:56 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,22 +6,6 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(100)
     ray_1.change(LedSpriteProperty.X, 1)
 })
-input.onButtonPressed(Button.B, function () {
-    ray_1 = game.createSprite(1, 2)
-    ray_1.change(LedSpriteProperty.X, 1)
-    basic.pause(100)
-    ray_1.change(LedSpriteProperty.X, 1)
-    basic.pause(100)
-    ray_1.change(LedSpriteProperty.X, 1)
-})
 let ray_1: game.LedSprite = null
 let fighter_1 = game.createSprite(0, 2)
-let fighter_2 = game.createSprite(5, 2)
-let fighter_1_health = 3
-let fighter_2_health = 3
-basic.forever(function () {
-	
-})
-basic.forever(function () {
-	
-})
+fighter_1 = game.createSprite(5, 2)
